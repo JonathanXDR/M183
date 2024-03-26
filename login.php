80f8823afb70de980fbd5c77271920d982ec1610
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2024-03-26 15:43:19 +0100

    Update login.php

--- login.php
+++ login.php
@@ -4,73 +4,53 @@ require_once 'fw/ElasticSearchLogger.php';
 
 $logger = new ElasticSearchLogger();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset ($_GET['username']) && isset ($_GET['password'])) {
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-    // Check connection
     if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    try {
-        $stmt = $conn->prepare("SELECT id, username, password FROM users WHERE username=?");
-        $stmt->bind_param("s", $username);
-        $stmt->execute();
-        $stmt->store_result();
-    } catch (Exception $e) {
-        $error = "Error: " . $e->getMessage();
-        $logger->log('error', 'SQL execution error', ['error' => $error]);
-        exit();
+        die ("Connection failed: " . $conn->connect_error);
     }
 
+    $stmt = $conn->prepare("SELECT id, username, password FROM users WHERE username=?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $stmt->store_result();
+
     if ($stmt->num_rows > 0) {
-        // Bind the result variables
         $stmt->bind_result($db_id, $db_username, $db_password);
-        // Fetch the result
         $stmt->fetch();
-        // Verify the password
+
         if ($password == $db_password) {
-            // Password is correct, store username in session
-            setcookie("username", $username, -1, "/"); // 86400 = 1 day
-            setcookie("userid", $db_id, -1, "/"); // 86400 = 1 day
-            // Redirect to index.php
-            $logger->log('info', 'User logged in successfully', ['username' => $username]);
+            setcookie("username", $username, -1, "/");
+            setcookie("userid", $db_id, -1, "/");
             header("Location: index.php");
             exit();
         } else {
-            // Password is incorrect
             echo "Incorrect password";
-            $logger->log('warning', 'User failed to log in', ['username' => $username, 'reason' => 'Incorrect password']);
+            $logger->log("ERROR", "Incorrect password attempt for user: $username");
         }
     } else {
-        // Username does not exist
         echo "Username does not exist";
-        $logger->log('warning', 'Failed login attempt', ['username' => $username, 'reason' => 'Username does not exist']);
+        $logger->log("WARN", "Login attempt for non-existent user: $username");
     }
 
-    // Close statement
     $stmt->close();
 }
 require_once 'fw/header.php';
 ?>
 
 <h2>Login</h2>
-
-<?php if (isset($error)) : ?>
-    <p>
-        <?php echo $error; ?>
-    </p>
-<?php endif; ?>
-
-<form id="form" method="get" action="<?php $_SERVER["PHP_SELF"]; ?>">
+<form id="form" method="get" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
     <div class="form-group">
         <label for="username">Username</label>
         <input type="text" class="form-control size-medium" name="username" id="username">
     </div>
     <div class="form-group">
         <label for="password">Password</label>
-        <input type="text" class="form-control size-medium" name="password" id="password">
+        <input type="password" class="form-control size-medium" name="password" id="password">
     </div>
     <div class="form-group">
         <label for="submit"></label>
