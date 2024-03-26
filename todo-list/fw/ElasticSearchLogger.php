657abf1584052c0dbfc1042fd7e6c61c8d59d79f
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2024-03-26 11:14:50 +0100

    Add ElasticSearchLogger

---
+++ todo-list/fw/ElasticSearchLogger.php
@@ -0,0 +1,28 @@
+<?php
+require 'vendor/autoload.php';
+
+use Elasticsearch\ClientBuilder;
+
+class ElasticSearchLogger
+{
+    private $client;
+
+    public function __construct()
+    {
+        $this->client = ClientBuilder::create()->setHosts(['localhost:9200'])->build();
+    }
+
+    public function log($level, $message, $context = [])
+    {
+        $data = [
+            'index' => 'application_logs',
+            'body' => [
+                'level' => $level,
+                'message' => $message,
+                'context' => $context,
+                'timestamp' => date('c'),
+            ]
+        ];
+        $this->client->index($data);
+    }
+}
