69997bd83483a737cd6654331c16c7759670f7d6
Jonathan Russ <jonathan.russ@edu.tbz.ch>
2024-03-28 14:24:52 +0100

    Fix config.php

--- config.php
+++ config.php
@@ -1,15 +1,14 @@
 <?php
 
-$basePath = dirname(__DIR__, 1);
-require_once $basePath . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 // Database credentials
-define('DB_HOST', $_ENV['DB_HOST']);
-define('DB_USER', $_ENV['DB_USER']);
-define('DB_PASS', $_ENV['DB_PASS']);
-define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_HOST', $_ENV['DATABASE_HOST']);
+define('DB_USER', $_ENV['DATABASE_USER']);
+define('DB_PASS', $_ENV['DATABASE_PASSWORD']);
+define('DB_NAME', $_ENV['DATABASE_NAME']);
 
 ?>
