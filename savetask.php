12bac46a8408a4beafd13231ab87e3ce7dca97d0
Pascal Rieder <pascal.rider@edu.tbz.ch>
2024-03-28 14:15:02 +0100

    Update savetask

--- savetask.php
+++ savetask.php
@@ -10,7 +10,9 @@ include 'fw/db.php';
 
 if (isset ($_POST['id']) && strlen($_POST['id']) != 0) {
   $id = $_POST["id"];
-  $stmt = executeStatement("select ID, title, state from tasks where ID = $id");
+  $stmt = $conn->prepare("select ID, title, state from tasks where ID = ?");
+  $stmt->bind_param("i", $id);
+  $stmt->execute();
   if ($stmt->num_rows == 0) {
     $id = "";
   }
@@ -23,10 +25,15 @@ if (isset ($_POST['title']) && isset ($_POST['state'])) {
   $userid = $_COOKIE['userid'];
 
   if ($id == "") {
-    $stmt = executeStatement("insert into tasks (title, state, userID) values ('$title', '$state', '$userid')");
+    $stmt = $conn->prepare("INSERT INTO tasks (title, state, userID) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $title, $state, $userid);
+    $stmt->execute();
   } else {
-    $stmt = executeStatement("update tasks set title = '$title', state = '$state' where ID = $id");
-  }
+    if ($id != "") {
+      $stmt = $conn->prepare("UPDATE tasks SET title = ?, state = ? WHERE ID = ?");
+      $stmt->bind_param("ssi", $title, $state, $id);
+      $stmt->execute();
+    }
 
   echo "<span class='info info-success'>Update successfull</span>";
 } else {
